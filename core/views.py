efb9851af7932d42c4d69767f9b9263edc0eaa0a
ZuluPro <montheanthony@hotmail.com>
2016-12-23 14:17:13 -0500

    Improved home views

--- core/views.py
+++ core/views.py
@@ -5,17 +5,28 @@ from askbot.models import Thread
 def home(request):
     if request.user.is_authenticated():
         return redirect('questions')
+    exclusion = []
+
     most_viewed_thread = Thread.objects.order_by('-view_count').first()
+    if most_viewed_thread:
+        exclusion.append(most_viewed_thread.id)
+
     most_answered_thread = Thread.objects.order_by('-answer_count')\
-        .exclude(id=most_viewed_thread.id)\
+        .exclude(id__in=exclusion)\
         .first()
+    if most_answered_thread:
+        exclusion.append(most_answered_thread.id)
+
     most_favourited_thread = Thread.objects.order_by('-favourite_count')\
-        .exclude(id__in=[most_viewed_thread.id, most_answered_thread.id])\
+        .exclude(id__in=exclusion)\
         .first()
+    if most_favourited_thread:
+        exclusion.append(most_favourited_thread.id)
+
     most_scored_thread = Thread.objects.order_by('-points')\
-        .exclude(id__in=[most_viewed_thread.id, most_answered_thread.id,
-                         most_favourited_thread.id])\
+        .exclude(id__in=exclusion)\
         .first()
+
     return render(request, 'home.html', {
         'most_viewed_thread': most_viewed_thread,
         'most_answered_thread': most_answered_thread,
